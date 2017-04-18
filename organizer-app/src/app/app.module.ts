16c877220eabf0856bf8aa9052deeaf2058a3d75
mkywong42 <mikiwong@ucsc.edu>
2017-04-18 13:49:05 -0700

    add calendar

--- organizer-app/src/app/app.module.ts
+++ organizer-app/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import {MdInputModule} from '../../node_modules/@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,9 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdInputModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
